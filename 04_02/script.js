cb87ef74e10c6cb876caaab2aa62cb8ac42c94e1
kirandeep123 <me@kiran.cc>
2019-07-01 16:07:17 +0530

    functions

---
+++ 04_02/script.js
@@ -0,0 +1,8 @@
+function findBiggestFraction(a,b){
+    console.log("the funtion is running ");
+a>b ?console.log("the value is " +a):console.log("the value is "+b);
+
+}
+var a =3/4;
+var b =6/7;
+findBiggestFraction(a,b);
